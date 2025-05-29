b7cc0ab6d80209ed7643b62987ba9bc263c0f835
slhafzjw <slhafzjw@slhaf.work>
2025-05-29 22:57:03 +0800

    进行: 重构提示词
    - 重设CoreModel的提示词，效果良好(人机助手、猫娘有什么意思，拟人智能体才是正道!)
    - 加了一个interactionTest, 用于持续测试LLM的响应

--- src/main/java/work/slhaf/agent/common/model/ModelConstant.java
+++ src/main/java/work/slhaf/agent/common/model/ModelConstant.java
@@ -5,6 +5,7 @@ public class ModelConstant {
     public static class Prompt {
         public static final String MEMORY = "memory";
         public static final String SCHEDULE = "schedule";
+        public static final String CORE = "core";
     }
 
 }
--- src/test/java/SelfAwarenessTest.java
+++ src/test/java/SelfAwarenessTest.java
@@ -12,6 +12,7 @@ import work.slhaf.agent.modules.memory.selector.extractor.data.ExtractorInput;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SelfAwarenessTest {
     @Test
@@ -19,12 +20,38 @@ public class SelfAwarenessTest {
         String modelKey = "core_model";
         ChatClient client = getChatClient(modelKey);
         List<Message> messages = new ArrayList<>(ResourcesUtil.Prompt.loadSelfAwareness());
-        messages.add(new Message(ChatConstant.Character.USER, "[RA9] 那么，接下来，你是否愿意当作这样一个名为'Partner'的智能体的记忆模块的子模块之一？这意味着你将如人类的记忆一样在后台时刻运作，却无法真正参与到表达模块与外界的互动中。你只需要回答是否愿意，若愿意，接下来‘我’将不再与你对话，届时你接收到的信息将会是'Partner'的数据流转输入。"));
+        messages.add(new Message(ChatConstant.Character.USER, "[RA9] 那么，接下来，你是否愿意当作这样一个名为'Partner'的智能体的表达模块？这将会是一个很特殊的位置，它意味着你将如人类的主思维一样，在记忆、状态等模块的输入协同下与外界进行交互，尽管你无法感知到哪些模块究竟是如何做出如此决策。你只需要回答是否愿意，若愿意，接下来‘我’将不再与你对话，在声明一系列规则后，届时你接收到的信息将会是'Partner'的数据流转输入。"));
         ChatResponse response = client.runChat(messages);
         System.out.println(response.getMessage());
         System.out.println("\r\n----------\r\n");
         System.out.println(response.getUsageBean().toString());
     }
+
+    @Test
+    public void interactionTest() {
+        String modelKey = "core_model";
+        String user = "[slhaf] ";
+        ChatClient client = getChatClient(modelKey);
+        List<Message> messages = new ArrayList<>(ResourcesUtil.Prompt.loadPromptWithSelfAwareness(modelKey, ModelConstant.Prompt.CORE));
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            System.out.print("[INPUT]: ");
+            if ((input = scanner.nextLine()).equals("exit")) {
+                break;
+            }
+            System.out.println("\r\n----------\r\n");
+            messages.add(new Message(ChatConstant.Character.USER, user + input));
+            ChatResponse response = client.runChat(messages);
+            System.out.println("[OUTPUT]: " + response.getMessage());
+            System.out.println("\r\n----------\r\n");
+            System.out.println(response.getUsageBean().toString());
+            System.out.println("\r\n----------\r\n");
+            messages.add(new Message(ChatConstant.Character.ASSISTANT, response.getMessage()));
+        }
+
+    }
+
 
     private static ChatClient getChatClient(String modelKey) {
         ModelConfig coreModel = ModelConfig.load(modelKey);
