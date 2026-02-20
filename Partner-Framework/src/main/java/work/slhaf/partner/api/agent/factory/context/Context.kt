115a8d54469b7952e1fbed9d1f50633badd8d262
slhafzjw <slhafzjw@slhaf.work>
2026-02-20 18:57:36 +0800

    refactor(framework): redesign `AgentContext` to store typed module contexts and runtime metadata

--- Partner-Framework/src/main/java/work/slhaf/partner/api/agent/factory/context/Context.kt
+++ Partner-Framework/src/main/java/work/slhaf/partner/api/agent/factory/context/Context.kt
@@ -2,26 +2,49 @@ package work.slhaf.partner.api.agent.factory.context
 
 import com.alibaba.fastjson2.JSONArray
 import work.slhaf.partner.api.agent.factory.module.abstracts.AbstractAgentModule
+import work.slhaf.partner.api.agent.runtime.interaction.flow.entity.RunningFlowContext
+import java.time.ZonedDateTime
 
 object AgentContext {
-
+    val modules = mutableMapOf<String, ModuleContextData<out AbstractAgentModule>>()
+    val capabilities = mutableMapOf<Class<Any>, Any>()
 }
 
 sealed class ModuleContextData<T : AbstractAgentModule> {
-    abstract val name: String
     abstract val clazz: Class<T>
     abstract val instance: T
-    abstract val prompt: JSONArray
-    abstract val modelActivated: Boolean
+    abstract val launchTime: ZonedDateTime
 
-    data class RunningModule<T : AbstractAgentModule>(
-        override val name: String,
+    val modelInfo: ModelInfo? = null
+    val metadata = mutableMapOf<String, Any>()
+
+    data class Running<T : AbstractAgentModule.Running<out RunningFlowContext>>(
         override val clazz: Class<T>,
         override val instance: T,
-        override val prompt: JSONArray,
-        override val modelActivated: Boolean,
+        override val launchTime: ZonedDateTime,
 
         val order: Int
     ) : ModuleContextData<T>()
 
+    data class Sub<T : AbstractAgentModule.Sub<out Any, out Any>>(
+        override val clazz: Class<T>,
+        override val instance: T,
+        override val launchTime: ZonedDateTime,
+
+        val injectTarget: MutableSet<AbstractAgentModule> = mutableSetOf()
+    ) : ModuleContextData<T>()
+
+    data class Standalone<T : AbstractAgentModule.Standalone>(
+        override val clazz: Class<T>,
+        override val instance: T,
+        override val launchTime: ZonedDateTime,
+
+        val injectTarget: MutableSet<AbstractAgentModule> = mutableSetOf()
+    ) : ModuleContextData<T>()
+
+    data class ModelInfo(
+        val baseUrl: String,
+        val model: String,
+        val basePrompt: JSONArray
+    )
 }
