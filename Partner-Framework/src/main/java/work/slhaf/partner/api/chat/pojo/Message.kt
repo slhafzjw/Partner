28a1bf8d1fd9fdd7cb231e4b3214d1efb83edb7b
slhafzjw <slhafzjw@slhaf.work>
2026-03-06 21:04:58 +0800

    refactor(chat): migrate `Message` from Java POJO to Kotlin data class

--- Partner-Framework/src/main/java/work/slhaf/partner/api/chat/pojo/Message.kt
+++ Partner-Framework/src/main/java/work/slhaf/partner/api/chat/pojo/Message.kt
@@ -1,22 +1,14 @@
-package work.slhaf.partner.api.chat.pojo;
+package work.slhaf.partner.api.chat.pojo
 
-import lombok.*;
-import work.slhaf.partner.api.common.entity.PersistableObject;
+import work.slhaf.partner.api.common.entity.PersistableObject
+import java.io.Serial
 
-import java.io.Serial;
-
-@EqualsAndHashCode(callSuper = true)
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Message extends PersistableObject {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
-
-    @NonNull
-    private String role;
-    @NonNull
-    private String content;
+data class Message(
+    val role: String,
+    val content: String
+) : PersistableObject() {
+    companion object {
+        @Serial
+        private const val serialVersionUID = 1L
+    }
 }
