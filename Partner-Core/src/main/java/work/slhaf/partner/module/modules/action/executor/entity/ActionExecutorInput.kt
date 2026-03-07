d9e384960fd0fc8751a57fab9fd2a6ab0ee6a060
slhafzjw <slhafzjw@slhaf.work>
2026-03-07 15:15:08 +0800

    refactor(action): remove ActionExecutorInput

--- Partner-Core/src/main/java/work/slhaf/partner/module/modules/action/executor/entity/ActionExecutorInput.kt
+++
@@ -1,5 +0,0 @@
-package work.slhaf.partner.module.modules.action.executor.entity
-
-import work.slhaf.partner.core.action.entity.ExecutableAction
-
-data class ActionExecutorInput(val actions: Set<ExecutableAction>)
