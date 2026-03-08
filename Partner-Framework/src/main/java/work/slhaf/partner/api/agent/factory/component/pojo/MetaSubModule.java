145aeed600c2498290c8667d51765ba362cf090e
slhafzjw <slhafzjw@slhaf.work>
2026-03-08 13:31:02 +0800

    refactor(Module): remove unused meta component POJOs from agent factory

--- Partner-Framework/src/main/java/work/slhaf/partner/api/agent/factory/component/pojo/MetaSubModule.java
+++
@@ -1,9 +0,0 @@
-package work.slhaf.partner.api.agent.factory.component.pojo;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-public class MetaSubModule extends BaseMetaModule<AbstractAgentSubModule> {
-}
