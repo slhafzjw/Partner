6aa96c33ac97f910d92661fba3dbc74bde2c23db
slhafzjw <slhafzjw@slhaf.work>
2026-03-22 13:50:18 +0800

    refactor(cognition): remove legacy user exception

--- Partner-Core/src/main/java/work/slhaf/partner/core/cognition/exception/UserNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package work.slhaf.partner.core.cognition.exception;
-
-public class UserNotExistsException extends RuntimeException {
-    public UserNotExistsException(String message) {
-        super(message);
-    }
-}
