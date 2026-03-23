556b8a5348de5629f24a158bb65a3b5473dc29f8
slhafzjw <slhafzjw@slhaf.work>
2026-03-23 23:19:16 +0800

    refactor(executor): remove legacy ActionRepairer、DynamicActionGenerator and related data class

--- Partner-Core/src/main/java/work/slhaf/partner/module/modules/action/executor/entity/GeneratorResult.java
+++
@@ -1,9 +0,0 @@
-package work.slhaf.partner.module.modules.action.executor.entity;
-
-import lombok.Data;
-import work.slhaf.partner.core.action.entity.MetaAction;
-
-@Data
-public class GeneratorResult {
-    private MetaAction tempAction;
-}
