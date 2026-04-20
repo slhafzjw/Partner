9c902e68d03554b305cc4019130e023b8bb9a4c4
slhafzjw <slhafzjw@slhaf.work>
2026-04-20 23:10:39 +0800

    fix(dto): add NoArgsConstructor to support formatted chat

--- Partner-Core/src/main/java/work/slhaf/partner/module/action/executor/entity/ExtractorResult.java
+++ Partner-Core/src/main/java/work/slhaf/partner/module/action/executor/entity/ExtractorResult.java
@@ -2,6 +2,7 @@ package work.slhaf.partner.module.action.executor.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class ExtractorResult {
 
     @Data
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class ParamEntry {
         private String name;
         private String value;
